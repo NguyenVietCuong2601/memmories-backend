81037c0a180aa38d87692d80794f735c2589fe4b
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-10-01 22:48:18 +0700

    update functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -8,11 +9,11 @@ export const getPosts = async (req, res) => {
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
-}
+};
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    
+
     const newPost = new PostMessage(post);
 
     try {
@@ -24,4 +25,19 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
-}
+};
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send("No post with that id");
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+        new: true,
+    });
+
+    res.json(updatedPost);
+};
