659b6238fdfc09f1b834d4487a6b6188a0d436bb
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-10-04 23:04:32 +0700

    make it better3

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
